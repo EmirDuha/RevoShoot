9123dace9e0a1bc720905a983329a0a8b9474817
Emie Duha Nakişci <emirduha93@gmail.com>
2025-10-05 23:55:47 +0300

    Game Over background

--- Assets/Scripts/PowerUpFall.cs
+++ Assets/Scripts/PowerUpFall.cs
@@ -6,10 +6,8 @@ public class PowerUpFall : MonoBehaviour
 
     void Update()
     {
-        // Aşağı hareket (y ekseni negatif yönde)
         transform.position += Vector3.down * fallSpeed * Time.deltaTime;
 
-        // Belirli bir yüksekliğin altına düşünce yok et
         if (transform.position.y < 0f)
         {
             Destroy(gameObject);
